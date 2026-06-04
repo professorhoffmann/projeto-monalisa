f175d6c1375aaa77f60abb9c4adb044f07869fcb
professorhoffmann <juliano.hoffmann@escola.pr.gov.br>
2026-06-03 22:10:03 -0300

    modificado sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,34 @@
 function setup() {
- createCanvas(400, 400);
- background(220);
+  createCanvas(400, 400);
+}
 
- // Rosto
- fill(255, 220, 180);
- ellipse(200, 200, 150, 200);
+let olhoX;
+let olhoY;
 
- // Olhos
- fill(0);
- ellipse(170, 180, 10, 10);
- ellipse(230, 180, 10, 10);
+function draw() {
+  background('#FF5722');
+  fill('blue');
+  fill('#03A9F4');
+  circle (200, 200, 300); //cabeça
+  fill('white');
+  circle (150, 150, 60);  //olho esquerdo
+  circle (250, 150, 60);  //olho direito
+  fill('black');
+  //circle (150, 150, 10); // Pupila esquerda
+  //circle (250, 150, 10); // Pupila direita
+    olhoX = map(mouseX, 0, 400, 130, 170);
+    olhoY = map(mouseY, 0, 400, 130, 170);
+  circle(olhoX, olhoY, 10); // nova pupila esquerda
+  circle(olhoX + 100, olhoY, 10); //nova pupila direita
+  line(150, 270, 250, 235); //boca
+  fill('#3F51B5');
+  triangle(200, 180, 170, 220, 220, 220); //nariz
+  line(123, 115, 178, 113); //sobrancelha direita
+  line(225, 116, 279, 106); //sobrancelha esquerda
+ 
+  
+  if(mouseIsPressed) {
+    console.log(mouseX, mouseY);
+  }
 
- // Boca (sorriso leve)
- noFill();
- stroke(0);
- arc(200, 220, 50, 30, 0, PI);
-
- // Cabelo
- fill(80, 50, 20);
- noStroke();
- arc(200, 150, 160, 180, PI, TWO_PI);
 }
